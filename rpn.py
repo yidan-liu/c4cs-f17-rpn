6cc7647c84b2437caf77075a8791138ce14e957a
Yidan Liu <yidan0257@gmail.com>
2017-11-06 19:50:14 -0500

    Added new code in rpn to demo coverage drop

--- rpn.py
+++ rpn.py
@@ -8,7 +8,8 @@ operators = {
     '-': operator.sub,
     '*': operator.mul,
     '/': operator.truediv,
-    '^': operator.pow
+    '^': operator.pow,
+    '%': operator.mod
 }
 
 def calculate(myarg):
